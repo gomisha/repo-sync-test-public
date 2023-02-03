42a965049801c96f93e0bf210fe7495e8ce53ff5
Misha <misha.rybalov@dapperlabs.com>
2023-02-03 23:22:48 +0700

    hello2 update 26

--- src/hello2.go
+++ src/hello2.go
@@ -3,53 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World 2!")
-
-	fmt.Println("Hello World 2 - from public repo!")
-
-	fmt.Println("Hello World 3 - from public repo!")
-
-	fmt.Println("Hello World 4 - from public repo!")
-
-	fmt.Println("Hello World 5 - from public repo!")
-
-	fmt.Println("Hello World 6 - from public repo!")
-
-	fmt.Println("Hello World 7 - from public repo!")
-
-	fmt.Println("Hello World 8 - from public repo!")
-
-	fmt.Println("Hello World 9 - from public repo!")
-
-	fmt.Println("Hello World 10 - from public repo!")
-
-	fmt.Println("Hello World 11 - from public repo!")
-
-	fmt.Println("Hello World 12 - from public repo!")
-
-	fmt.Println("Hello World 13 - from public repo!")
-
-	fmt.Println("Hello World 14 - from public repo!")
-
-	fmt.Println("Hello World 15 - from public repo!")
-
-	fmt.Println("Hello World 16 - from public repo!")
-
-	fmt.Println("Hello World 17 - from public repo!")
-
-	fmt.Println("Hello World 18 - from public repo!")
-
-	fmt.Println("Hello World 19 - from public repo!")
-
-	fmt.Println("Hello World 20 - from public repo!")
-
-	fmt.Println("Hello World 21 - from public repo!")
-
-	fmt.Println("Hello World 22 - from public repo!")
-
-	fmt.Println("Hello World 23 - from public repo!")
-
-	fmt.Println("Hello World 24 - from public repo!")
-
-	fmt.Println("Hello World 25 - from public repo!")
+	fmt.Println("Hello World 26 - from public repo!")
 }
